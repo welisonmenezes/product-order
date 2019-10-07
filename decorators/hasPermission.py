cbcd426476bc3f1e0ff2b4aac358cc389574f2ce
welisonmenezes <welisonmenezes@hotmail.com>
2019-10-07 20:12:48 -0300

    Add pass backup

--- decorators/hasPermission.py
+++ decorators/hasPermission.py
@@ -1,6 +1,9 @@
 from flask import url_for, redirect, session, flash
 from functools import wraps
 
+# senha para iniciar
+# $2b$12$BxwpzOUqT8s4QnzRzHxzpuXXNTTqtJzxPzce8rpjXlDB.92sd0yKW (123456)
+
 def login_required(f):
     @wraps(f)
     def wrap(*args, **kwargs):
