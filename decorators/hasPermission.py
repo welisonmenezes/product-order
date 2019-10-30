54efe5e63e7a32a4adb323af804c40b4e729317c
welisonmenezes <welisonmenezes@hotmail.com>
2019-10-30 11:54:14 -0300

    Add password to backup

--- decorators/hasPermission.py
+++ decorators/hasPermission.py
@@ -2,7 +2,7 @@ from flask import url_for, redirect, session, flash
 from functools import wraps
 
 # senha para iniciar
-# $2b$12$BxwpzOUqT8s4QnzRzHxzpuXXNTTqtJzxPzce8rpjXlDB.92sd0yKW (123456)
+# $2b$12$HLmXrD04OduCBeqEqW9Cje69DVAXdhie6E7Ts7RAEthit.te7uxwu (123456)
 
 def login_required(f):
     @wraps(f)
